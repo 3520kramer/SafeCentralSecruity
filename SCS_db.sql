a636b0139e9fbe5a5982c6cd80c82db5f4456f06
Kiwilynn <42769363+Kiwilynn@users.noreply.github.com>
2019-05-13 15:11:15 +0200

    Script2.0
    First draft of the finished DB

--- SCS_db.sql
+++ SCS_db.sql
@@ -4,6 +4,18 @@
 CREATE DATABASE IF NOT EXISTS SCS_db;
 USE SCS_db;
 
+CREATE TABLE byer
+(
+postnummer INT PRIMARY KEY NOT NULL, 
+bydel VARCHAR(30) NOT NULL
+);
+
+INSERT INTO byer VALUES
+('2300', 'København S'),
+('2620', 'Albertslund'),
+('2450', 'København SV'),
+('2670', 'Greve');
+
 CREATE TABLE brugere
 (
 id  INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -14,12 +26,12 @@ status 		VARCHAR(10) NOT NULL DEFAULT 'user'
 
 INSERT INTO brugere VALUES
 (DEFAULT, 'Admin', 'Admin', 'Admin'),
-
 (DEFAULT, 'Kagebageren', '1234', DEFAULT),
 (DEFAULT, 'Hansen31', '1234', DEFAULT),
 (DEFAULT, 'Krammermanden', '1234', DEFAULT),
 (DEFAULT, 'Sindzz01', '1234', DEFAULT),
 (DEFAULT, 'Brormanden', '1234', DEFAULT);
+
 
 CREATE TABLE kunder
 (
@@ -28,15 +40,19 @@ firma_navn 		VARCHAR(30) NOT NULL,
 kontaktperson 	VARCHAR(30) NOT NULL,
 telefon 		VARCHAR(12) NOT NULL,
 email 			VARCHAR(50) NOT NULL,
-CVR 			VARCHAR(10)
+CVR 			VARCHAR(10),
+addresse		VARCHAR(30) NOT NULL,
+postnummer		INT			NOT NULL,
+
+FOREIGN KEY(postnummer) REFERENCES byer(postnummer)
 );
 
 INSERT INTO kunder VALUES
-(DEFAULT, 'Skohjørnet', 'Hans Christensen', '20201919', 'hansen@sko.dk', '87654321'),
-(DEFAULT, 'Hyggekrogen', 'Miklas Narrov', '25251515', 'MI@Hyg.dk', '38959301'),
-(DEFAULT, 'Kongburger', 'Snu Satan', '33336119', 'haha@ha.dk', '83421521'),
-(DEFAULT, 'WeAr', 'Jens Andersen', '43153919', 'JA@wear.dk', '84235121'),
-(DEFAULT, 'qtOutfit', 'Hildur Jensen', '25151239', 'HJ@qt.dk', '63412321');
+(DEFAULT, 'Skohjørnet', 'Hans Christensen', '20201919', 'hansen@sko.dk', '87654321', 'Lygten 70', '2300'),
+(DEFAULT, 'Hyggekrogen', 'Miklas Narrov', '25251515', 'MI@Hyg.dk', '38959301', 'Rødovrevej', '2620'),
+(DEFAULT, 'Kongburger', 'Snu Satan', '33336119', 'haha@ha.dk', '83421521', 'Vesterbrovej 60', '2450'),
+(DEFAULT, 'WeAr', 'Jens Andersen', '43153919', 'JA@wear.dk', '84235121', 'Magrevej 50', '2670'),
+(DEFAULT, 'qtOutfit', 'Hildur Jensen', '25151239', 'HJ@qt.dk', '63412321', 'Kagevej 125', '2300');
 
 CREATE TABLE medarbejder
 (
@@ -47,12 +63,34 @@ ansættelsesdato	DATE NOT NULL,
 telefon			VARCHAR(10) NOT NULL,
 email			VARCHAR(30),
 cpr				VARCHAR(15) NOT NULL,
-løn				DECIMAL(10.2) NOT NULL DEFAULT '20000.00'
+løn				DECIMAL(10.2) NOT NULL DEFAULT '20000.00',
+addresse 		VARCHAR(30) NOT NULL,
+postnummer 		INT	NOT NULL,
+
+FOREIGN KEY(postnummer)references byer(postnummer)
 );
 
 INSERT INTO medarbejder VALUES
-(DEFAULT, 'Svend', 'Petersen', '2015-05-05', '50502929', 'Svend@hotmail.dk', '200590-9597', DEFAULT),
-(DEFAULT, 'Jens', 'Ibsen', '2018-10-10', '45402929', 'Jeib@hotmail.dk', '100791-9595', DEFAULT),
-(DEFAULT, 'Mads', 'Direksen', '2017-07-03', '65342929', 'Direk@gmail.dk', '200590-9597', DEFAULT),
-(DEFAULT, 'Henrik', 'Vardik', '2014-01-10', '32342929', 'Vardikkeren@gmail.dk', '200790-2595', DEFAULT),
-(DEFAULT, 'Hildur', 'Bentsen', '2011-12-15', '25602929', 'Hidu@hotmail.dk', '200590-5596', DEFAULT);
+(DEFAULT, 'Svend', 'Petersen', '2015-05-05', '50502929', 'Svend@hotmail.dk', '200590-9597', DEFAULT, 'Vejensvej 50', '2300'),
+(DEFAULT, 'Jens', 'Ibsen', '2018-10-10', '45402929', 'Jeib@hotmail.dk', '100791-9595', DEFAULT, 'Bakerstreet 122', '2620'),
+(DEFAULT, 'Mads', 'Direksen', '2017-07-03', '65342929', 'Direk@gmail.dk', '200590-9597', DEFAULT, 'Nalerstreet 697', '2670'),
+(DEFAULT, 'Henrik', 'Vardik', '2014-01-10', '32342929', 'Vardikkeren@gmail.dk', '200790-2595', DEFAULT, 'Lavervej 10', '2450'),
+(DEFAULT, 'Hildur', 'Bentsen', '2011-12-15', '25602929', 'Hidu@hotmail.dk', '200590-5596', DEFAULT, 'ajgervej 601', '2300');
+
+
+CREATE TABLE vagtplan
+(
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+starttid TIME,
+sluttid TIME,
+timetal DECIMAL(3.2),
+dato DATE,
+medarbejder_id_fk INT,
+kunder_id_fk INT,
+
+FOREIGN KEY (medarbejder_id_fk)				REFERENCES medarbejder(id),
+FOREIGN KEY (kunder_id_fk)						REFERENCES kunder(id)
+);
+
+INSERT INTO vagtplan VALUES
+(DEFAULT, '23:00:00', '05:00:00', '6.0', '2015-05-05', '1', '1');
