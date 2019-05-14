4dba4c1640bd0036186db07cea47da043c1dcfef
Kiwilynn <kasll@hotmail.com>
2019-05-14 12:31:35 +0200

    HomeController now autowired as well (Missed from earlier oops)
    ViewCustomer html now fully functional
    
    Index button fixed to a postmapping

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,10 +1,21 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Customer;
+import com.example.demo.Service.Services;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController{
+
+
+    @Autowired
+    Services services;
 
     @GetMapping("/")
     public String index(){
@@ -20,4 +31,13 @@ public class HomeController{
     public String viewCustomer(){
         return "/customer/viewCustomer";
     }
+
+
+    @PostMapping("/customer/viewCustomer")
+    public String viewCustomers(Model model) {
+        List<Customer> customerList = services.getAll();
+        model.addAttribute("customers", customerList);
+        return "customer/viewCustomer";
+    }
+
 }
