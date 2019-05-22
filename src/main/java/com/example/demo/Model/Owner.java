9279700fd2820c41a33b0b85bc72fc214df1df2f
MadsRF <madsrunefrederiksen@gmail.com>
2019-05-22 10:39:18 +0200

    Lille ændring - løn til timeløn

--- src/main/java/com/example/demo/Model/Owner.java
+++ src/main/java/com/example/demo/Model/Owner.java
@@ -1,4 +1,10 @@
 package com.example.demo.Model;
 
 public class Owner extends User {
+
+    @Override
+    public String getAnsettelsesdato() {
+        String ansettelsesdato = "2018-01-01";
+        return ansettelsesdato;
+    }
 }
